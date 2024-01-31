b8f2fdc35aa47d4a3cf9d847ab4c21ecabf42768
oleksandrMO <oleksandrmorhun1991@gmail.com>
2024-01-31 15:25:29 +0000

    js_first_change

---
+++ fe-pro-29.01.2024/01/main.js
@@ -0,0 +1,13 @@
+// First Task
+let userNickname='Alexander';
+let userScore=32;
+
+// Second Task
+let Price = prompt('Ціна покупки');
+let Quantity = prompt('кількість продуктів');
+
+console.log('price', Price);
+console.log('quantity', Quantity);
+
+let together = (+Price * +Quantity);
+console.log(together);
